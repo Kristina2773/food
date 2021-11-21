279d22a08837ab97f9bd7ab69ed037b257924dde
Kristina <d.kristina27@yandex.ru>
2021-11-21 13:06:10 +0400

    append fetch

--- js/script.js
+++ js/script.js
@@ -186,124 +186,124 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 }
 
-new MenuCard (
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-     9,
-     '.menu .container',
-     'menu__item',
-     'big'
+    new MenuCard (
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container',
+        'menu__item',
+        'big'
 
+        ).render();
+
+
+    new MenuCard (
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        15,
+        '.menu .container',
+        'menu__item',
+        'big'
+        
+        ).render();
+
+    new MenuCard (
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        21,
+        '.menu .container',
+        'menu__item',
+        'big'
+            
     ).render();
 
+    //Forms
 
-new MenuCard (
-    "img/tabs/elite.jpg",
-    "elite",
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-     15,
-     '.menu .container',
-     'menu__item',
-     'big'
-    
-    ).render();
+    const forms = document.querySelectorAll('form');
 
-new MenuCard (
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-     21,
-    '.menu .container',
-    'menu__item',
-    'big'
-        
-).render();
+    const message= {
+        loading: 'img/form/spinner.svg',
+        success: 'Спасибо, мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
 
-//Forms
-
-const forms = document.querySelectorAll('form');
-
-const message= {
-    loading: 'img/form/spinner.svg',
-    success: 'Спасибо, мы с вами свяжемся',
-    failure: 'Что-то пошло не так...'
-};
-
-forms.forEach(item => {
-    postData(item);
-});
-
-function postData(form) {
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-
-        const statusMessage = document.createElement('img');
-        statusMessage.src = message.loading;
-        statusMessage.style.cssText = `
-            display: block;
-            margin: 0 auto;        
-        `;
-        form.insertAdjacentElement('afterend', statusMessage);
-
-        const request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-
-        //request.setRequestHeader('Content-type', "multipart/form-data");
-        request.setRequestHeader('Content-type', "application/json");
-        const formData = new FormData(form);
-
-        const object = {};
-        
-        formData.forEach(function(value, key){
-            object[key] = value;
-        });
-
-        const json = JSON.stringify(object);
-        
-        request.send(json);
-
-        request.addEventListener('load', () => {
-            if(request.status === 200) {
-                console.log(request.response);
-                showThanksModal(message.success);
-                form.reset();
-                statusMessage.remove();
-            } else {
-                showThanksModal(message.failure);
-            }
-        });
+    forms.forEach(item => {
+        postData(item);
     });
-}
 
-function showThanksModal(message) {
-    const prevModalDialog = document.querySelector('.modal__dialog');
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
 
-    prevModalDialog.classList.add('hide');
-    showModalContent();
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;        
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
 
-    const thanksModal = document.createElement('div');
-    thanksModal.classList.add('modal__dialog');
+        
+            const formData = new FormData(form);
 
-    thanksModal.innerHTML = `
-        <div class = "modal__content"> 
-            <div class = "modal__close" data-close>&times;</div>   
-            <div class = "modal__title">${message}</div>     
-        </div>    
-    `;
+            const object = {};
+            
+            formData.forEach(function(value, key){
+                object[key] = value;
+            }); 
+            
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch( () => {
+                showThanksModal(message.failure);
+            }).finally( () => {
+                form.reset();
+            });
 
-    document.querySelector('.modal').append(thanksModal);
+        });
+    }
 
-    setTimeout( () => {
-        thanksModal.remove();
-        prevModalDialog.classList.remove('hide');
-        prevModalDialog.classList.add('show');
-        closeModalContent();
-    }, 4000);
-}
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        showModalContent();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+
+        thanksModal.innerHTML = `
+            <div class = "modal__content"> 
+                <div class = "modal__close" data-close>&times;</div>   
+                <div class = "modal__title">${message}</div>     
+            </div>    
+        `;
+
+        document.querySelector('.modal').append(thanksModal);
+
+        setTimeout( () => {
+            thanksModal.remove();
+            prevModalDialog.classList.remove('hide');
+            prevModalDialog.classList.add('show');
+            closeModalContent();
+        }, 4000);
+    }
     
 });
 
